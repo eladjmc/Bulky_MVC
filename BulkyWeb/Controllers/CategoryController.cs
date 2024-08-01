bf323b555e3a9ece7d611d79b3975f362652b080
EladWebiks <elad_harel@webiks.com>
2024-08-01 09:39:11 +0300

    made some small changes, nothing major

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -9,7 +9,7 @@ namespace BulkyWeb.Controllers
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
-            _db=db;
+            _db = db;
 
         }
         public IActionResult Index()
@@ -20,6 +20,40 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Create()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(newCategory);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null) return NotFound();
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(newCategory);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
